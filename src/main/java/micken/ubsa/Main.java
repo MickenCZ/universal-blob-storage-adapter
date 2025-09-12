19375afe8322b5c2f4bf3810bb26b1449e5df0fa
Michael Cirkl <MickenCZ@seznam.cz>
2025-09-12 19:24:27 +0200

    initial commit

--- src/main/java/micken/ubsa/Main.java
+++ src/main/java/micken/ubsa/Main.java
@@ -1,7 +1,8 @@
-package org.example;
+package micken.ubsa;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
